9aec3a029e9b13afa1c358ea0f73eb4a59b74212
brockp <brockp@users.noreply.github.com>
2022-09-14 18:13:49 +0000

    Update platform deliverables yayy

---
+++ build/ios/Classes/Generated/StyleDictionaryIcons.h
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionaryIcons.h
+//
+// Do not edit directly
+// Generated on Wed, 14 Sep 2022 18:13:47 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+@interface StyleDictionaryIcons : NSObject
++ (NSArray *)values;
+@end
---
+++ build/ios/Classes/Generated/StyleDictionarySize.m
@@ -0,0 +1,12 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Wed, 14 Sep 2022 18:13:47 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
